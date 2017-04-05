b4672592b558bd9c234943e4baa05bbaa8b4457d
Joseph Cotton <jtcotton63@gmail.com>
2017-04-04 21:34:06 -0600

    Upvote to server works

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,26 +1,37 @@
 angular.module('comment', [])
-.controller('MainCtrl', ['$scope', function($scope) {
+.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
     
-    $scope.comments = [
-        {title:'Comment 1', upvotes:5},
-        {title:'Comment 2', upvotes:6},
-        {title:'Comment 3', upvotes:1},
-        {title:'Comment 4', upvotes:4},
-        {title:'Comment 5', upvotes:3}
-    ];
+    $scope.comments = [];
     
     $scope.addComment = function() {
-        $scope.comments.push({
-            title:$scope.formContent,
-            upvotes:0
-        });
+        var comment = {
+            title: $scope.formContent,
+            upvotes: 0
+        };
         
-        $scope.formContent = '';
+        $http.post('/comments', comment).success(function(data) {
+            $scope.formContent = '';
+            $scope.getAll();
+        });
     };
     
     $scope.incrementUpvotes = function(comment) {
-        comment.upvotes += 1;
+        $http.put('/comments/' + comment._id + '/upvotes')
+        .success(function(data){
+            $scope.getAll();
+        });
     };
+    
+    $scope.getAll = function() {
+        return $http.get('/comments').success(function(data) {
+            angular.copy(data, $scope.comments);
+        });
+      };
+      
+    angular.element(document).ready(function () {
+        $scope.getAll();
+        $scope.$apply();
+    });
     
   }
 ]);
