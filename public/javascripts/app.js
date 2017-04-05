c120d653169e473b52c72c93c237666db1c3c5ff
Joseph Cotton <jtcotton63@gmail.com>
2017-04-04 19:31:17 -0600

    Upvotes working correctly

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,26 @@
 angular.module('comment', [])
-.controller('MainCtrl', [
-  '$scope',
-  function($scope){
-    $scope.test = 'Hello world!';
+.controller('MainCtrl', ['$scope', function($scope) {
+    
+    $scope.comments = [
+        {title:'Comment 1', upvotes:5},
+        {title:'Comment 2', upvotes:6},
+        {title:'Comment 3', upvotes:1},
+        {title:'Comment 4', upvotes:4},
+        {title:'Comment 5', upvotes:3}
+    ];
+    
+    $scope.addComment = function() {
+        $scope.comments.push({
+            title:$scope.formContent,
+            upvotes:0
+        });
+        
+        $scope.formContent = '';
+    };
+    
+    $scope.incrementUpvotes = function(comment) {
+        comment.upvotes += 1;
+    };
+    
   }
 ]);
